a0f40ba73d150dfd96d376c85d0554fec9f4237d
Oleh Koval <mleekko@gmail.com>
2023-11-19 22:18:23 +0200

    Simple vs lazy loading pages.

---
+++ src/main/java/com/mleekko/radix/page/RadixDappToolkitPageApplication.java
@@ -0,0 +1,13 @@
+package com.mleekko.radix.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixDappToolkitPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadixDappToolkitPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mleekko/radix/page/RadixDappToolkitPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mleekko.radix.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadixDappToolkitPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
